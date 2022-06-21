e0797ad397f2682f57640974e8ff7b9060735f1b
vivanov <brightsdayss@gmail.com>
2022-06-21 12:20:48 +0400

    add json-server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import SearchFilter from './components/SearchFilter'
 import NumbersList from './components/NumbersList'
 import AddForm from './components/AddForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newPerson, setNewPerson] = useState({name: '',number: ''})
   const [filter, setFilter] = useState('')
   const [alert, setAlert] = useState(false)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  })
 
   const handleChange = (event, type) => {
     setAlert(false)
