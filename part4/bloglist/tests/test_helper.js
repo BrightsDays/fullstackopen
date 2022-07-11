f0266f831e22275ed2a20666860aa7d02499141b
vivanov <brightsdayss@gmail.com>
2022-07-11 17:47:44 +0400

    add authorization

--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -20,6 +21,11 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
 const nonExistingId = async () => {
   const blog = new Blog({
     title: 'Post 4',
@@ -37,5 +43,6 @@ const nonExistingId = async () => {
 module.exports = {
   initialBlogs,
   blogsInDb,
+  usersInDb,
   nonExistingId
 }
