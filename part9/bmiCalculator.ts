4a34a01b33195d9b474b1fe5979510b9feba67a0
vivanov <brightsdayss@gmail.com>
2022-08-16 12:47:29 +0400

    add bmi calc

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,30 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = (weight / Math.pow(height, 2)) * 10000
+
+  if (bmi < 16) {
+    return('Underweight (Severe thinness)');
+  }
+  if (bmi >= 16 && bmi < 17) {
+    return('Underweight (Moderate thinness)');
+  }
+  if (bmi >= 17 && bmi < 18.5) {
+    return('Underweight (Mild thinness)');
+  }
+  if (bmi >= 18.5 && bmi < 25) {
+    return('Normal (healthy weight)');
+  }
+  if (bmi >= 25 && bmi < 30) {
+    return('Overweight (Pre-obese)');
+  }
+  if (bmi >= 30 && bmi < 35) {
+    return('Obese (Class I)');
+  }
+  if (bmi >= 35 && bmi < 40) {
+    return('Obese (Class II)');
+  }
+  if (bmi >= 40) {
+    return('Obese (Class III)');
+  }
+};
+
+console.log(calculateBmi(175, 75));
