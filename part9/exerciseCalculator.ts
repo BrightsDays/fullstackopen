b349d9b33902a10992c06c468745f65ff3e32e89
vivanov <brightsdayss@gmail.com>
2022-08-18 11:33:08 +0400

    add args

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,41 +1,64 @@
-type RatingDescription = 'not too bad but could be better' | 'too bad' | 'very good'
+type RatingDescription = 'not too bad but could be better' | 'too bad' | 'very good';
 
 interface Result {
-  periodLength: number,
-  trainingDays: number,
-  success: boolean,
-  rating: number,
-  ratingDescription: RatingDescription,
-  target: number,
-  average: number
-}
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: RatingDescription;
+  target: number;
+  average: number;
+};
+
+interface ExerciseValues {
+  target: number;
+  days: number[];
+};
+
+const parseExerciseArguments = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  
+  if (args.filter(arg => isNaN(Number(arg))).length === 2) {
+    let exerciceDays = []
+    for (let i = 3; i < args.length; i++) {
+      exerciceDays.push(args[i])
+    }
+    
+    return {
+      target: Number(args[3]),
+      days: exerciceDays
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  };
+};
 
 const calculateExercises = (exerciceHours: Array<number>, target: number): Result => {
-  const periodLength: number = exerciceHours.length
+  const periodLength: number = exerciceHours.length;
 
   const trainingDays: number = exerciceHours
-    .filter((day: number) => day !== 0).length
+    .filter((day: number) => day !== 0).length;
 
   const average: number = exerciceHours
-    .reduce((a: number, b: number): number => a + b, 0) / exerciceHours.length
+    .reduce((a: number, b: number): number => a + b, 0) / exerciceHours.length;
 
-  const success: boolean = average > target
+  const success: boolean = average > target;
 
-  let rating: number = 2
+  let rating: number = 2;
   if (average > target) {
-    rating = 3
-  }
+    rating = 3;
+  };
   if (average < (target / 2)) {
-    rating = 1
-  }
+    rating = 1;
+  };
 
-  let ratingDescription: RatingDescription = 'not too bad but could be better'
+  let ratingDescription: RatingDescription = 'not too bad but could be better';
   if (rating === 1) {
-    ratingDescription = 'too bad'
-  }
+    ratingDescription = 'too bad';
+  };
   if (rating === 3) {
-    ratingDescription = 'very good'
-  }
+    ratingDescription = 'very good';
+  };
 
   return {
     periodLength,
@@ -45,7 +68,16 @@ const calculateExercises = (exerciceHours: Array<number>, target: number): Resul
     ratingDescription,
     target,
     average
-  }
-}
+  };
+};
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+  const { target, days } = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(days, target));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
