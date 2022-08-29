e20dcaef8fdc1e3c31a484ff472e6d13dee3544b
vivanov <brightsdayss@gmail.com>
2022-08-29 10:38:40 +0400

    add validation

---
+++ part9/patientor-backend/src/utils.ts
@@ -0,0 +1,69 @@
+import { Gender, NewPatientEntry } from "../types";
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const parseName = (name: unknown): string => {
+  if (!name || !isString(name)) {
+    throw new Error('Incorrect or missing name');
+  }
+
+  return name;
+};
+
+const parseOccupation = (occupation: unknown): string => {
+  if (!occupation || !isString(occupation)) {
+    throw new Error('Incorrect or missing occupation');
+  }
+
+  return occupation;
+};
+
+const parseSsn = (ssn: unknown): string => {
+  if (!ssn || !isString(ssn)) {
+    throw new Error('Incorrect or missing ssn');
+  }
+
+  return ssn;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('Incorrect or missing date');
+  }
+
+  return date;
+};
+
+const isGender = (param: any): param is Gender => {
+  return Object.values(Gender).includes(param);
+};
+
+const parseGender = (gender: unknown): string => {
+  if (!gender || !isString(gender) || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender');
+  }
+
+  return gender;
+};
+
+type Fields = { name: unknown, gender: unknown, ssn: unknown, dateOfBirth: unknown, occupation: unknown };
+
+const toNewPatientEntry = ({ name, gender, ssn, dateOfBirth, occupation }: Fields): NewPatientEntry => {
+  const newEntry: NewPatientEntry = {
+    name: parseName(name),
+    gender: parseGender(gender),
+    ssn: parseSsn(ssn),
+    dateOfBirth: parseDate(dateOfBirth),
+    occupation: parseOccupation(occupation)
+  };
+
+  return newEntry;
+};
+
+export default toNewPatientEntry;
